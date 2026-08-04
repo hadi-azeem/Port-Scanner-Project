1de2e4da6bc6f951f9f202ffbec807324af44940
Hadi Azeem <mhadiazeem103@gmail.com>
2026-08-04 14:38:13 +0500

    Added port range scanning with loop

--- scanner.py
+++ scanner.py
@@ -1,17 +1,26 @@
 import socket
 
 IP_ADDRESS = '127.0.0.1'
-PORT = 8000
-ADDR = (IP_ADDRESS, PORT)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+open_count = 0
+closed_count = 0
+open_ports = []
 
-s.settimeout(1)
+for port in range(1, 1025):
+    ADDR = (IP_ADDRESS, port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
 
-result = s.connect_ex((ADDR))
+    result = s.connect_ex((ADDR))
 
-if result == 0:
-    print("Port is open")
-else:
-    print("Port is closed")
+    if result == 0:
+        open_count += 1
+        open_ports.append(port)
+    else:
+        closed_count += 1
 
-s.close()
+    s.close()
+
+print("Scan complete")
+print(f"No. of ports open: {open_count}")
+print(f"No. of ports closed: {closed_count}")
+print(f"Open ports: {open_ports}")
