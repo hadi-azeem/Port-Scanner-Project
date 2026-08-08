cf7d671b8cf94dadf2cb99ee1b6fabbe56ccc4f7
Hadi Azeem <mhadiazeem103@gmail.com>
2026-08-08 20:20:06 +0500

    Added argparse CLI arguments for IP  and port range

--- scanner.py
+++ scanner.py
@@ -1,7 +1,18 @@
 import socket
 import threading
+import argparse
 
-IP_ADDRESS = '127.0.0.1'
+parser = argparse.ArgumentParser(description="TCP Port Scanner")
+parser.add_argument("ip", help="Target IP address")
+parser.add_argument("--ports", help="Port range e.g. 1-1024", default="1-1024")
+
+args = parser.parse_args()
+
+start_port, end_port = args.ports.split("-")
+start_port = int(start_port)
+end_port = int(end_port)
+
+IP_ADDRESS = args.ip
 open_ports = []
 lock = threading.Lock()
 port_banners = {}
@@ -25,7 +36,7 @@ def scan_port(ip, port):
 
 threads = []
 
-for port in range(1, 9000):
+for port in range(start_port, end_port + 1):
     t = threading.Thread(target=scan_port, args=(IP_ADDRESS, port))
     threads.append(t)
     t.start()
